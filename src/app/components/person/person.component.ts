e0fc277cd709cd0740c5532b7eace0d675d921ca
Sven Glauser <sven.glauser@divtec.ch>
2022-04-05 19:05:29 +0200

    Create PersonComponent

---
+++ src/app/components/person/person.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input} from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {Person} from "../../interfaces/search/person/person";
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent {
+
+  @Input() person: Person = {
+    adult: false,
+    id: 0,
+    known_for: [],
+    media_type: "",
+    name: "",
+    popularity: 0,
+    profile_path: ""
+  };
+
+  constructor() { }
+
+  getImage() {
+    // TODO enlever l'index et créer un objet json
+    return environment.images.url + environment.images.profile_sizes[2] + this.person.profile_path;
+  }
+}
