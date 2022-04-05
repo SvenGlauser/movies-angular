ffbab51294874e21feadb785726ff29de2efbc8c
Sven Glauser <sven.glauser@divtec.ch>
2022-04-05 12:17:17 +0200

    Mettre en place le projet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,46 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    key: "15f9d7fec19ec6b5b9bc491acd508fe6",
+    url: "https://api.themoviedb.org/3/",
+  },
+  images: {
+    url: "https://image.tmdb.org/t/p/",
+    backdrop_sizes: [
+      "w300",
+      "w780",
+      "w1280",
+      "original"
+    ],
+    logo_sizes: [
+      "w45",
+      "w92",
+      "w154",
+      "w185",
+      "w300",
+      "w500",
+      "original"
+    ],
+    poster_sizes: [
+      "w92",
+      "w154",
+      "w185",
+      "w342",
+      "w500",
+      "w780",
+      "original"
+    ],
+    profile_sizes: [
+      "w45",
+      "w185",
+      "h632",
+      "original"
+    ],
+    still_sizes: [
+      "w92",
+      "w185",
+      "w300",
+      "original"
+    ]
+  }
 };
