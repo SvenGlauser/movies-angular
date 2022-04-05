9df86e60b06d75bef21a986eae65ac53a83043c2
Sven Glauser <sven.glauser@divtec.ch>
2022-04-05 18:39:01 +0200

    Search on input change

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {TmdbApiService} from "../../services/api/tmdb-api.service";
 import {Result} from "../../interfaces/search/result/result";
 
@@ -7,7 +7,7 @@ import {Result} from "../../interfaces/search/result/result";
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
   private result: Result = {page: 0, results: [], total_pages: 0, total_results: 0};
 
@@ -15,8 +15,8 @@ export class SearchComponent implements OnInit {
 
   constructor(public tmdbApiService: TmdbApiService) { }
 
-  async search(): Promise<void> {
-    this.result = <Result>(await this.tmdbApiService.search("tom cruise").toPromise());
+  async search(value: string): Promise<void> {
+    this.result = <Result>(await this.tmdbApiService.search(value).toPromise());
     this.type = this.getTypes().length > 0 ? this.getTypes()[0] : "";
   }
 
@@ -27,9 +27,5 @@ export class SearchComponent implements OnInit {
   getResults() {
     let result = this.result.results;
     return result.filter(value => value.media_type === this.type);
-  }
-
-  ngOnInit(): void {
-    this.search().then();
   }
 }
