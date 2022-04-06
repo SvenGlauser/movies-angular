6fb925b9b3998c4f606d7ea325676c05004a83d7
Sven Glauser <sven.glauser@divtec.ch>
2022-04-06 10:53:28 +0200

    Open result in view

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -4,6 +4,7 @@ import {Result} from "../../interfaces/search/result/result";
 import {Tv} from "../../interfaces/search/tv/tv";
 import {Movie} from "../../interfaces/search/movie/movie";
 import {Person} from "../../interfaces/search/person/person";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -16,11 +17,16 @@ export class SearchComponent {
 
   type: string = "";
 
-  constructor(public tmdbApiService: TmdbApiService) { }
+  constructor(private tmdbApiService: TmdbApiService,
+              private router: Router) { }
 
   async search(value: string): Promise<void> {
     this.result = <Result>(await this.tmdbApiService.search(value).toPromise());
     this.type = this.getTypes().length > 0 ? this.getTypes()[0] : "";
+  }
+
+  open(resultElement: Movie | Tv | Person) {
+      this.router.navigate(["view/" + resultElement.media_type + "/" + resultElement.id]);
   }
 
   getTypes() {
