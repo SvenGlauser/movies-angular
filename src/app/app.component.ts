eb0a49159f958e1c3affd9134e38edbf6b3fdc84
Sven Glauser <sven.glauser@divtec.ch>
2022-05-25 10:47:28 +0200

    Récupérer la langue du navigateur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {TranslateService} from "@ngx-translate/core";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,25 @@ import {TranslateService} from "@ngx-translate/core";
 })
 export class AppComponent {
   constructor(private translate: TranslateService) {
-    translate.setDefaultLang(localStorage.getItem('language') || 'fr-FR');
-    translate.use(localStorage.getItem('language') || 'fr-FR');
+    let lang = this.getLanguage();
+    localStorage.setItem('language', lang);
+    translate.setDefaultLang(lang);
+    translate.use(lang);
+  }
+
+  getLanguage(): string {
+    let languageArray = Object.keys(environment.api.languages);
+
+    if (languageArray.includes(localStorage.getItem('language') || "")) {
+      return String(localStorage.getItem('language'));
+    } else if (languageArray.includes(navigator.language)) {
+      return navigator.language;
+    } else if (languageArray.filter(lang => lang.startsWith(navigator.language)).length > 0) {
+      return languageArray.filter(lang => lang.startsWith(navigator.language))[0];
+    } else if (navigator.languages.filter(lang => languageArray.includes(lang))) {
+      return navigator.languages.filter(lang => languageArray.includes(lang))[0];
+    } else {
+      return 'fr-FR';
+    }
   }
 }
